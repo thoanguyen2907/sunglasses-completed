40d2c20c56042eb9d3d9903e11069e8d011a7469
thoanguyen2907 <thoanguyen290795@gmail.com>
2022-11-24 22:37:18 +0200

    first commit

--- src/Sunglasses/Sunglasses.js
+++ src/Sunglasses/Sunglasses.js
@@ -1,26 +1,44 @@
 import React, { useState } from 'react'
 import dataGlasses from '../Data/dataGlasses.json';
+  
+const styleGlasses = {
+    width: '150px',
+    top: '75px',
+    right: '115px',
+    opacity: '0.7',
+    transform: 'rotate(0deg)',
+    position: "absolute"
+}
+
+const infoGlasses = {
+    width: '250px',
+    top: '200px',
+    left: '318px',
+    paddingLeft: '15px',
+    backgroundColor: 'rgba(255,127,0,.5)',
+    textAlign: 'left',
+    height: '105px', 
+    position: "relative"
+}
 
 export default function Sunglasses() {
-  
-    const styleGlasses = {
-        width: '150px',
-        top: '75px',
-        right: '117px',
-        opacity: '0.7',
-        transform: 'rotate(0deg)'
+    const [glassesCurrent, setGlassesCurrent] = useState({
+        "id": 2,
+        "price": 50,
+        "name": "GUCCI G8759H",
+        "url": "./glassesImage/v2.png",
+        "desc": "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. "
+    })
+   const renderGlassesList = () => {
+        return dataGlasses.map((glassesItem, index) => {
+            return <img onClick={()=>{changeGlasses(glassesItem) }} className="ml-2 p-2 border border-width-1" style={{ width: '110px', cursor: 'pointer' }} key={index} src={glassesItem.url} />
+        })
     }
 
-    const infoGlasses = {
-        width: '250px',
-        top: '200px',
-        left: '318px',
-        paddingLeft: '15px',
-        backgroundColor: 'rgba(255,127,0,.5)',
-        textAlign: 'left',
-        height: '105px'
+    const changeGlasses = (newGlasses) => {
+        setGlassesCurrent(newGlasses)
     }
-   
+
 
     return (
         <div style={{ backgroundImage: 'url(./glassesImage/background.jpg)', backgroundSize: '2000px', minHeight: '2000px' }}>
@@ -31,12 +49,12 @@ export default function Sunglasses() {
                         <div className="col-6">
                             <div className="position-relative">
                                 <img className="position-absolute" style={{ width: '250px' }} src="./glassesImage/model.jpg" alt='model.jpg' />
-                                <img style={styleGlasses} className="position-absolute glassesStyle" src="./glassesImage/v1.png" />
+                                <img style={styleGlasses} className="glassesStyle" src={glassesCurrent.url} />
 
-                                <div style={infoGlasses} className="position-relative ">
-                                    <span style={{ color: '#AB82FF', fontSize: '17px' }} className="font-weight-bold"> PRADA</span> <br />
+                                <div style={infoGlasses}>
+                                    <span style={{ color: '#AB82FF', fontSize: '17px' }} className="font-weight-bold"> {glassesCurrent.name}</span> <br />
                                     <span style={{ fontSize: '14px', paddingRight: '5px', fontWeight: '400' }}>
-                                    Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. 
+                                    {glassesCurrent.desc} 
                                     </span>
                                 </div>
                             </div>
@@ -47,10 +65,10 @@ export default function Sunglasses() {
                         </div>
                     </div>
                 </div>
-                {/* Div chứa các kính được chọn */}
+          
                 <div className="bg-light container text-center mt-5 d-flex justify-content-center p-5">
           
-            
+                {renderGlassesList()}
                 </div>
             </div>
         </div>
